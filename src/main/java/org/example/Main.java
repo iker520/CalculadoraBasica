af022781fcc36c950d9909f2fa1501e7c8411209
Iker <igomezg05@educantabria.es>
2025-11-27 19:15:09 +0100

    Primer commit: calculadora basica

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Calculadora calcular = new Calculadora();
+        int opcion=0, num1=0, num2=0;
+        boolean entradaCorrecta=false;
+        do {
+            try {
+                System.out.print("Introduce el primer número: ");
+                num1 = sc.nextInt();
+                System.out.print("Introduce el segundo número: ");
+                num2 = sc.nextInt();
+                entradaCorrecta=true;
+            } catch (InputMismatchException e) {
+                System.out.println("Error: ingresa numeros, no letras.");
+                entradaCorrecta=false;
+                sc.nextLine();
+            }
+        }while(!entradaCorrecta);
+        do {
+            try {
+                System.out.println("Elige la operación:");
+                System.out.println("1. Sumar");
+                System.out.println("2. Restar");
+                System.out.println("Otro numero. Salir");
+                opcion = sc.nextInt();
+                if (opcion == 1) {
+                    System.out.println("Resultado: " + calcular.sumar(num1, num2));
+                } else if (opcion == 2) {
+                    System.out.println("Resultado: " + calcular.restar(num1, num2));
+                }
+            } catch (ArithmeticException e) {
+                System.out.println("Sintax Error");
+                sc.nextLine();
+            } catch (Exception e) {
+                System.out.println("Error");
+                sc.nextLine();
+            }
+        } while (opcion == 1 | opcion == 2);
+        sc.close();
+    }
+}
